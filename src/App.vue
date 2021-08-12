0351d6c1e215ce360e54f772113289d5ebd4fae6
Vaishnavi Zurange <vaishnavizurange20@gmail.com>
2021-08-12 16:34:54 +0530

    Add existing file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{title}}</h1>
+    <p>Welcome...</p>
+    <!-- header and text are the props here and props are used to pass the data from parent to children component  -->
+    <div v-if="showmodel">
+       <!-- <model :header="header" :text="text" theme="sale" @close="togglemodel"/> -->
+
+       <model theme="sale" @close="togglemodel"> 
+         <!-- the template that we pass inside here is known as slot.it gets passe as a slot to the model componenet -->
+         <h1>sign up for giveaway</h1>
+         <p>grab at half price</p>
+
+            <!-- named slots -->
+
+         <template v-slot:links> 
+           <a href="">Sign up now</a>
+           <a href="">More info</a>
+         </template>
+       </model>
+
+    </div>
+
+    <div v-if="showmodel2">
+
+       <model theme="" @close="togglemodel2"> 
+         <h1>sign up to the newsletter</h1>
+         <p>For update and promo </p>
+       </model>
+
+    </div>
+
+    <button @click.alt="togglemodel"> Open Modal(alt)</button> <br> <br>
+    <button @click="togglemodel2"> Open Modal two</button> <br> <br>
+    <input type="text" ref="names"> 
+    <button @click="handleclk">click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import model from './components/model.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+    title:'My first vue app :)',
+    // header:'sign up for the giveaway',
+    // text:'grab your giveaway at half price',
+    showmodel: false,
+    showmodel2:false
+    }
+  },
+  methods:{
+    handleclk(){
+      console.log(this.$refs.names)
+      this.$refs.names.classList.add('active')
+      this.$refs.names.focus()
+    },
+    togglemodel(){
+      this.showmodel=!this.showmodel
+    },
+    togglemodel2(){
+      this.showmodel2=!this.showmodel2
+    }
+  },
   components: {
-    HelloWorld
+    model
   }
 }
 </script>
@@ -22,5 +76,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  display: inline-block;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
 }
 </style>
---
+++ src/components/model.vue
@@ -0,0 +1,74 @@
+<template>
+   <div class="backdrop" @click.self="closemodule">
+       <div class="model" :class="{ sale: theme=== 'sale'}">
+           <!-- {{header}} -->
+           <!-- {{text}} -->
+           <!-- defaultslot -->
+           <!-- default content will only show up if there is no content to pass in slot -->
+          <slot>Default content</slot> 
+          <div class="action">
+              <!-- named slot -->
+            <slot name="links"></slot>
+          </div>
+       </div>
+   </div> 
+</template>
+
+<script>
+export default {
+  props: ['header','text','theme'],
+  methods:{
+      closemodule(){
+          //emitting custome events
+          this.$emit('close')
+      }
+  }
+}
+</script>
+
+<style>
+    .model{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .model h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    .model .action{
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .model .action a{
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
+    .model.sale{
+        background: crimson;
+        color: white;
+    }
+    .model.sale h1{
+        color: white;
+    }
+    .model.sale .action{
+        color: white;
+    }
+    .model.sale .action a{
+        color: white;
+    }
+</style>
